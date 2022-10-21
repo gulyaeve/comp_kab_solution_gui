ce02de2f7dd182e6dc1a2c3fa9f6820aeff74350
gulyaeve <gulyaev.e@icloud.com>
2022-10-21 10:32:32 +0300

    Фикс логов

--- app.py
+++ app.py
@@ -7,15 +7,18 @@ from config import hosts_file_path, config_path
 from system import this_host, user, run_command
 from teacher_control import TeacherWindow
 
+
+# Настройка логирования
+logging.basicConfig(
+    filename='log.txt',
+    format=u'%(asctime)s %(filename)s [LINE:%(lineno)d] [%(funcName)s()] #%(levelname)-15s %(message)s',
+    level=logging.INFO,
+)
+
+
 # Создание папки с конфигом
 run_command(f'mkdir -p {config_path}')
 run_command(f'touch {hosts_file_path}')
-
-# Настройка логирования
-logging.basicConfig(filename='log.txt',
-                    format=u'%(asctime)s %(filename)s [LINE:%(lineno)d] [%(funcName)s()] #%(levelname)-15s %(message)s',
-                    level=logging.INFO,
-                    )
 
 
 if __name__ == '__main__':
