906bd913b15f0e97a27e01192c035eff360f897c
gulyaeve <gulyaev.e@icloud.com>
2022-10-22 16:15:56 +0300

    Добавлена валидация имени хоста регуляркой

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ config_path = f'misc'
 # config_path = f'/home/{user}/.teacher_control'
 hosts_file_path = f'{config_path}/hosts.json'
 version = "1.0"
-hostname_expression = r"(s[cmnpt][\w\d]*)-([\w\d]*)-([\w\d]*)-([\w\d]*).local"
+hostname_expression = r"(s[cmnpt][\w\d]+)(-[\w\d]+){1,3}.local"
