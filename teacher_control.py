31dfb217dcb7c2bfe64c5ae9e572ad0e62e55a82
gulyaeve <gulyaev.e@icloud.com>
2022-10-22 19:17:31 +0300

    Добавлено автообновление окна учителя

--- teacher_control.py
+++ teacher_control.py
@@ -58,9 +58,6 @@ class TeacherWindow(QWidget):
 
         self.hosts_items = QListWidget()
         self.hosts_items.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        hosts_from_file = self.hosts.to_list()
-        self.hosts_items.addItems(hosts_from_file)
-        self.n = len(self.hosts_items)
         grid.addWidget(self.hosts_items, 2, 1, 5, 2)
 
         self.move(300, 150)
@@ -74,6 +71,7 @@ class TeacherWindow(QWidget):
             self.hosts = Hosts()
             hosts_from_file = self.hosts.to_list()
             self.hosts_items.addItems(hosts_from_file)
+            self.n = len(self.hosts_items)
 
     def select_all(self):
         for i in range(self.n):
